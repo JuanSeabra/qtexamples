385ecf9cfeb12e3e2b73f604d1ce27065d5f92a7
Waldomiro Juan Gadelha Seabra <wjuan.06@gmail.com>
2018-12-17 21:24:10 -0400

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,50 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <iostream>
+#include<vector>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+QStandardItemModel* MainWindow::createTreeViewModel(int itens){
+    QStandardItemModel *model = new QStandardItemModel();
+    QStandardItem *dad, *son;
+    int step = 0, next_step = 0, step_size=1000000;
+    while(step < itens){
+        next_step += 100;
+        if(next_step > itens){
+            dad = new QStandardItem(QString::number(step)+".."+QString::number(itens));
+        } else {
+            dad = new QStandardItem(QString::number(step)+".."+QString::number(step+100));
+        }
+
+        model->appendRow(dad);
+        for (; step < next_step && step < itens; ++step){
+            son = new QStandardItem(QString::number(step));
+            dad->appendRow(son);
+        }
+    }
+    return model;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    int number = ui->lineEdit->text().toInt();
+    std::cout << "valor: " << number << std::endl;
+    QStandardItemModel* model = createTreeViewModel(number);
+    ui->treeView->setModel(model);
+}
+
+void MainWindow::on_lineEdit_returnPressed()
+{
+    on_pushButton_clicked();
+}
